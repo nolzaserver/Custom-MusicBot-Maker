6dff45b0418fe7e5db9cdbc7fa821d6e688172a8
playerdecuple <playerdecuple@gmail.com>
2020-10-06 06:38:18 +0900

    Added GuildMusicManager

---
+++ src/main/java/com/projectdecuple/Core/Music/GuildMusicManager.java
@@ -0,0 +1,21 @@
+package com.projectdecuple.Core.Music;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+
+public class GuildMusicManager {
+    public AudioPlayer pl;
+    public TrackScheduler scheduler;
+    public AudioPlayerSendHandler sendHandler;
+
+    public GuildMusicManager(AudioPlayerManager manager) {
+        pl = manager.createPlayer();
+        scheduler = new TrackScheduler(pl);
+        sendHandler = new AudioPlayerSendHandler(pl);
+        pl.addListener(scheduler);
+    }
+
+    public AudioPlayerSendHandler getSendHandler() {
+        return new AudioPlayerSendHandler(pl);
+    }
+}
