2d3cf28dcda042805527fe52bcc24ff01eedbbd1
playerdecuple <playerdecuple@gmail.com>
2020-11-21 16:44:46 +0900

    Update imported classes

--- src/main/java/com/projectdecuple/Core/GetJSON.java
+++ src/main/java/com/projectdecuple/Core/GetJSON.java
@@ -1,7 +1,6 @@
 package com.projectdecuple.Core;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
